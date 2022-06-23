9af41e0a718add75cb1617d2159a35e238ebc39d
Chirag Jain <coder2543j@gmail.com>
2022-06-23 21:05:47 +0530

    D

--- Kelly/assets/js/main.js
+++ Kelly/assets/js/main.js
@@ -40,6 +40,7 @@
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
+    console.log(offset);
 
     let elementPos = select(el).offsetTop
     window.scrollTo({
